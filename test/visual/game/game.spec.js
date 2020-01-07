efa43994b15ac6339188c841006c756f648527cd
Fernando Navarro <fnp.formacion@gmail.com>
2020-01-07 20:46:14 +0100

    Visual Test. move to the right, left and up. #11

--- test/visual/game/game.spec.js
+++ test/visual/game/game.spec.js
@@ -19,7 +19,7 @@ describe('scenes/game', () => {
     await expect(await page.content()).toMatchSnapshot();
   }, 10000);
 
-  it('should match visual screenshot', async () => {
+  it('should match visual screenshot (init)', async () => {
     await page.waitFor(27000); // 27 Seconds. All stars must be stopped
     const image = await page.screenshot();
     const maxDiff = 700// bomb
@@ -31,4 +31,45 @@ describe('scenes/game', () => {
     await expect(image).toMatchImageSnapshot(options);
   }, 30000); // 30 Seconds
 
+  it('should match visual screenshot when move to the right', async () => {
+    for (let index = 0; index < 5; index++) {
+      await page.keyboard.press('ArrowRight', {delay: 1000});
+    }
+    const image = await page.screenshot();
+    const maxDiff = 700 + 300;// bomb + pixels of player
+    const options = {
+      customSnapshotIdentifier: 'game-arrow-right.png',
+      failureThreshold: maxDiff, 
+      failureThresholdType: 'pixel'
+    }
+    await expect(image).toMatchImageSnapshot(options);
+  }, 10000); // 10 Seconds
+
+  it('should match visual screenshot when move to the left', async () => {
+    for (let index = 0; index < 5; index++) {
+      await page.keyboard.press('ArrowLeft', {delay: 1000});
+    }
+    const image = await page.screenshot();
+    const maxDiff = 700 + 300;// bomb + pixels of player
+    const options = {
+      customSnapshotIdentifier: 'game-arrow-left.png',
+      failureThreshold: maxDiff, 
+      failureThresholdType: 'pixel'
+    }
+    await expect(image).toMatchImageSnapshot(options);
+  }, 10000); // 10 Seconds
+
+  it('should match visual screenshot when move to up', async () => {
+    for (let index = 0; index < 5; index++) {
+      await page.keyboard.press('ArrowUp', {delay: 1000});
+    }
+    const image = await page.screenshot();
+    const maxDiff = 700 + 300;// bomb + pixels of player
+    const options = {
+      customSnapshotIdentifier: 'game-arrow-up.png',
+      failureThreshold: maxDiff, 
+      failureThresholdType: 'pixel'
+    }
+    await expect(image).toMatchImageSnapshot(options);
+  }, 10000); // 10 Seconds  
 })
