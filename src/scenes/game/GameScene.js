0fb4762d27d5626ef4a1078354043b5f4164ce7e
Fernando Navarro <kfern@users.noreply.github.com>
2020-01-07 16:28:24 +0100

    [BTG] Change image preload name
    #13

--- src/scenes/game/GameScene.js
+++ src/scenes/game/GameScene.js
@@ -32,7 +32,7 @@ export default class GameScene extends Phaser.Scene {
     // Images
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
-    this.load.image('star', 'assets/star.png');
+    this.load.image('stars', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 
